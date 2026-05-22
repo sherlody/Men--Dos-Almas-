2ccaa5e19cce1d4642b12ccc0b8f149f22670a94
azoka345 <alexvvm545@gmail.com>
2026-05-22 14:34:11 -0600

    eliminar api

--- Dos Almas Back/routes/api.php
+++ Dos Almas Back/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\API\ProductosController;
+use App\Http\Controllers\ProductosController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/menu', [ProductosController::class, 'getMenu']);
